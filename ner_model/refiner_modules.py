30073cd08ff0d366660cd3d5aed64f2995b4b661
YOONNAJANG <morelychee@gmail.com>
2023-02-08 16:50:25 +0900

    Yoonna modified

--- ner_model/refiner_modules.py
+++ ner_model/refiner_modules.py
@@ -47,28 +47,13 @@ class BartEncDec(BartForConditionalGeneration):
 
 
         output_dict = dict()
-        # print("input_ids.size(): ",input_ids.size()) # [2, 312] -> [bos] [knoweldge token] gk [persona token] ps [human token] history(last)
-        # print("cls_labels.size(): ",ner_labels.size()) # [2, 312]
-        # print(input_ids)
+
         outputs = self.model(input_ids=input_ids, decoder_input_ids=decoder_input_ids)
         lm_logits = self.lm_head(outputs[0]) + self.final_logits_bias  # batch, decseqlen, dim
         ner_logits_cls = outputs['encoder_last_hidden_state']  # batch, encseqlen, dim
         ner_logits = self.summary(ner_logits_cls).squeeze(-1)
         output_dict['ner_logits'] = ner_logits
 
-
-        #
-        #
-        # hst_index = (input_ids == 50266).nonzero(as_tuple=True)[1]
-        # # print("******** hst_idex: ", hst_index)
-        # # outputs = self.model(input_ids=input_ids, decoder_input_ids=decoder_input_ids)
-        # # lm_logits = self.lm_head(outputs[0]) + self.final_logits_bias #batch, decseqlen, dim
-        # # ner_logits_cls = outputs['encoder_last_hidden_state'] #batch, encseqlen, dim
-        # #
-        # # ner_logits_cls = ner_logits_cls[:,:hst_index]
-        # # ner_labels = ner_labels[:,:hst_index]
-        # # ner_logits =self.summary(ner_logits_cls).squeeze(-1)
-        # # output_dict['ner_logits'] = ner_logits
 
 
         masked_lm_loss = None
@@ -79,20 +64,10 @@ class BartEncDec(BartForConditionalGeneration):
 
         ner_loss = None
         if ner_labels is not None:
-
-            # loss_fct = CrossEntropyLoss(weight =self.normedWeights, ignore_index=-1)
             loss_fct = CrossEntropyLoss(ignore_index=-1)
-            # cls_loss = loss_fct(cls_logits, cls_labels.type_as(cls_logits))
-
-
             ner_loss = loss_fct(ner_logits.view(-1, 3), ner_labels.view(-1).long())
             predictions = torch.argmax(ner_logits, dim=2)
-
             ner_acc = 0
-            # print(predictions)
-            # print(ner_labels)
-
-
             true_predictions = [
                 [self.id2label[p.item()] for (p, l) in zip(prediction, label) if l != -1]
                 for prediction, label in zip(predictions, ner_labels)
@@ -101,10 +76,6 @@ class BartEncDec(BartForConditionalGeneration):
                 [self.id2label[l.item()] for (p, l) in zip(prediction, label) if l != -1]
                 for prediction, label in zip(predictions, ner_labels)
             ]
-            # print(true_predictions)
-            # print(true_labels)
-            # print("----------------")
-
             results = self.metric.compute(predictions=true_predictions, references=true_labels)
             ner_results = {
           "precision": results["overall_precision"],
@@ -112,7 +83,6 @@ class BartEncDec(BartForConditionalGeneration):
           "f1": results["overall_f1"],
           "accuracy": results["overall_accuracy"],
       }
-
 
             output_dict['lm_logits'] = lm_logits
             output_dict['ner_loss'] = ner_loss
@@ -148,9 +118,7 @@ class BartEncDec_NER_txt(BartForConditionalGeneration):
             ):
 
         output_dict = dict()
-        # print("input_ids.size(): ",input_ids.size()) # [2, 312] -> [bos] [knoweldge token] gk [persona token] ps [human token] history(last)
-        # print("cls_labels.size(): ",ner_labels.size()) # [2, 312]
-        # print(input_ids)
+
         if inputs_embeds == None:
             outputs = self.model(input_ids=input_ids, decoder_input_ids=decoder_input_ids)
         else:
@@ -207,20 +175,13 @@ class BartEncDec_NER_txt(BartForConditionalGeneration):
             output_dict['lm_loss'] = masked_lm_loss
 
 
-
         ner_loss = None
         if ner_labels is not None:
-
-            # loss_fct = CrossEntropyLoss(weight =self.normedWeights, ignore_index=-1)
             loss_fct = CrossEntropyLoss(ignore_index=-1)
             # cls_loss = loss_fct(cls_logits, cls_labels.type_as(cls_logits))
             ner_loss = loss_fct(ner_logits.view(-1, 3), ner_labels.view(-1).long())
             predictions = torch.argmax(ner_logits, dim=2)
-
             ner_acc = 0
-            # print(predictions)
-            # print(ner_labels)
-
             true_predictions = [
                 [self.id2label[p.item()] for (p, l) in zip(prediction, label) if l != -1]
                 for prediction, label in zip(predictions, ner_labels)
@@ -229,10 +190,6 @@ class BartEncDec_NER_txt(BartForConditionalGeneration):
                 [self.id2label[l.item()] for (p, l) in zip(prediction, label) if l != -1]
                 for prediction, label in zip(predictions, ner_labels)
             ]
-            # print(true_predictions)
-            # print(true_labels)
-            # print("----------------")
-
             results = self.metric.compute(predictions=true_predictions, references=true_labels)
             ner_results = {
           "precision": results["overall_precision"],
@@ -241,9 +198,7 @@ class BartEncDec_NER_txt(BartForConditionalGeneration):
           "accuracy": results["overall_accuracy"],
       }
 
-
-            # output_dict['lm_logits'] = lm_logits
             output_dict['ner_loss'] = ner_loss
             output_dict['ner_results'] = ner_results
-            # breakpoint()
+
         return output_dict
